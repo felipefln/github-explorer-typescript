d251072d4cda7cae469f188342b1eb42f95da162
felipefln <felipecarvalho_22_@hotmail.com>
2020-04-29 14:49:15 -0300

    Create form dashboard page, add logo git-explorer.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
     return (
-        <Title>Explore repositórios no Github</Title>
+        <React.Fragment>
+            <img src={logoImg} alt="github explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form >
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+        </React.Fragment>
+
     )
 }
 
