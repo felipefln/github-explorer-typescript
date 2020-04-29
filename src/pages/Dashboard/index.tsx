bc21de6ef90533e9195da3339e7e8996a11df5e2
felipefln <felipecarvalho_22_@hotmail.com>
2020-04-29 15:12:49 -0300

    add stylesheet dashboard page list repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
     return (
@@ -14,6 +15,37 @@ const Dashboard: React.FC = () => {
                 <input placeholder="Digite o nome do repositório" />
                 <button type="submit">Pesquisar</button>
             </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars1.githubusercontent.com/u/33233733?s=460&u=1c90b55f42fd870be976adc663dd4d7af836a22d&v=4" alt="git-img" />
+
+                    <div>
+                        <strong>felipefln/github</strong>
+                        <p>Github repositorios list</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img src="https://avatars1.githubusercontent.com/u/33233733?s=460&u=1c90b55f42fd870be976adc663dd4d7af836a22d&v=4" alt="git-img" />
+
+                    <div>
+                        <strong>felipefln/github</strong>
+                        <p>Github repositorios list</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+                <a href="teste">
+                    <img src="https://avatars1.githubusercontent.com/u/33233733?s=460&u=1c90b55f42fd870be976adc663dd4d7af836a22d&v=4" alt="git-img" />
+
+                    <div>
+                        <strong>felipefln/github</strong>
+                        <p>Github repositorios list</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+            </Repositories>
         </React.Fragment>
 
     )
