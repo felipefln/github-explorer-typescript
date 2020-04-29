4f9b254ffce60f79d14f803fa48b18880215878c
felipefln <felipecarvalho_22_@hotmail.com>
2020-04-29 13:36:13 -0300

    Create global style.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes';
 
-const App: React.FC = () => <BrowserRouter><Routes /></BrowserRouter>
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+    <React.Fragment>
+        <GlobalStyle />
+        <BrowserRouter><Routes /></BrowserRouter>
+    </React.Fragment>
+
+)
+
+
 
 export default App;
